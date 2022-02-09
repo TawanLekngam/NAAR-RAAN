6bacff375e7300885a0e800cbec140be8efcb820
tawancraft <tawancraft@outlook.com>
2022-02-10 05:37:02 +0700

    create product addon

--- models/product_addon.py
+++ models/product_addon.py
@@ -1,1 +1,13 @@
-from abc import ABC
+from dataclasses import dataclass
+
+
+@dataclass
+class AddOn:
+    __name: str
+    __price: float
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_price(self) -> float:
+        return self.__price
