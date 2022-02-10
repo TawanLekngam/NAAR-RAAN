09a62afb790567323f0d848a051e15cf5fc6661d
tawancraft <tawancraft@outlook.com>
2022-02-11 02:29:44 +0700

    uodate core

--- Application.py
+++ Application.py
@@ -1,5 +1,14 @@
 # Application file for run program
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+def main() -> int:
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.showFullScreen()
+    return app.exec()
 
 
 if __name__ == "__main__":
-    pass
+    sys.exit(main())
