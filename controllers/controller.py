6e988a9e5eb28d4d63ac4f0d03a48775cb73b7f2
tawancraft <tawancraft@outlook.com>
2022-02-11 02:30:37 +0700

    remove unuse import

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,12 +4,10 @@ from models.model import Model
 
 
 class Controller(ABC):
-    __administrator:bool
 
-    def __init__(self, view: QWidget, model: Model,admin = False) -> None:
+    def __init__(self, view: QWidget, model: Model) -> None:
         self.view = view
         self.model = model
-        self.__administrator = admin
 
     def show_page(self) -> None:
         self.view.showFullScreen()
