de90c7778b5cfd05f710a59355952b8727f2fa54
beammi <beamratchwalee@gmail.com>
2022-02-15 16:04:31 +0700

    Added Icon

--- views/log_in.py
+++ views/log_in.py
@@ -1,4 +1,5 @@
 import sys
+from tkinter import CENTER
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
@@ -13,32 +14,30 @@ class Log_In(QWidget):
         self.gridLayout.setObjectName(u"gridLayout")
         self.label_username = QLabel("USERNAME",self)
         self.label_username.setObjectName(u"label_username")
-        
-
-        self.gridLayout.addWidget(self.label_username, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_username, 0, 0,1,3)
 
         self.lineEdit_username = QLineEdit(self)
         self.lineEdit_username.setObjectName(u"lineEdit_username")
 
-        self.gridLayout.addWidget(self.lineEdit_username, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit_username, 0, 1,1,2)
 
         self.label_password = QLabel("PASSWORD",self)
         self.label_password.setObjectName(u"label_password")
-
-        self.gridLayout.addWidget(self.label_password, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_password, 1, 0,1,3)
 
         self.lineEdit_password = QLineEdit(self)
         self.lineEdit_password.setObjectName(u"lineEdit_password")
 
-        self.gridLayout.addWidget(self.lineEdit_password, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit_password, 1, 1,1,2)
 
         self.pushButton_login = QPushButton(self)
         self.pushButton_login.setObjectName(u"pushButton_login")
-        pixmap = QPixmap("views/assets/Image/coffee.png")
-        self.pushButton_login.setIcon(pixmap)
+        self.pixmap = QPixmap("C:/Users/Beam/OneDrive/Desktop/SEP/SEP_Project/views/assets/Image/coffee.png")
+        self.pushButton_login.setIcon(self.pixmap)
         self.pushButton_login.setIconSize(QSize(55,55))
+        
 
-        self.gridLayout.addWidget(self.pushButton_login, 2, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_login, 2, 0,1,3)
 
         self.show()
     def set_styleSheet(self, file_name: str):
