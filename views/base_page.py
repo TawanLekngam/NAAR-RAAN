50df4f72e01d80d8784e1987f118940f8ab8451e
tawancraft <tawancraft@outlook.com>
2022-02-17 03:42:44 +0700

    Update protected method

--- views/base_page.py
+++ views/base_page.py
@@ -1,12 +1,13 @@
 from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QFont
 
 
 class BasePage(QWidget):
     def __init__(self, parent: QWidget):
         QWidget.__init__(self, parent)
-        self.set_stylesheet()
+        self._set_stylesheet()
 
-    def set_stylesheet(self):
+    def _set_stylesheet(self):
         file_name = type(self).__name__.lower() + ".qss"
         theme_dir_name = "themes"
         with open(theme_dir_name + '/' + file_name, "r") as file:
