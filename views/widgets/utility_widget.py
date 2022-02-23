b35a2d2f9a0f2435bcf550a94fbcde426b086772
Tawan <tawancraft@outlook.com>
2022-02-24 02:21:12 +0700

    Fix error code

--- views/widgets/utility_widget.py
+++ views/widgets/utility_widget.py
@@ -2,12 +2,12 @@ from PySide6.QtWidgets import (QWidget)
 from PySide6.QtGui import (QFont)
 
 
-class Widget(QWidget()):
+class Widget(QWidget):
 
     def __init__(self, parent: QWidget):
-        QWidget.__init__(self, QWidget)
+        QWidget.__init__(self, parent)
 
-    def _get_font(self,size: font) -> QFont:
+    def _get_font(self,size: int) -> QFont:
         font = QFont()
         font.setPixelSize(size)
         return font
