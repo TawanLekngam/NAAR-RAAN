fd8f93cb5acc514a08150faa1c36fc5cd682fcdf
Tawan <tawancraft@outlook.com>
2022-02-24 10:08:10 +0700

    Create UserTab and MenuItem

--- views/widgets/utility_widget.py
+++ views/widgets/utility_widget.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import (QWidget)
-from PySide6.QtGui import (QFont)
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton)
+from PySide6.QtGui import (QFont, QMouseEvent)
+
+# Base Dynamic widget
 
 
 class Widget(QWidget):
@@ -7,7 +9,27 @@ class Widget(QWidget):
     def __init__(self, parent: QWidget):
         QWidget.__init__(self, parent)
 
-    def _get_font(self,size: int) -> QFont:
+    def _get_font(self, size: int) -> QFont:
         font = QFont()
         font.setPixelSize(size)
         return font
+
+
+class UserTab(Widget):
+
+    def __init__(self, parent: QWidget):
+        Widget.__init__(self, parent)
+        layout = QVBoxLayout()
+        btn = QPushButton("click me", self)
+        layout.addWidget(btn)
+        self.setLayout(layout)
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        if self.underMouse():
+            print("Log: On me.")
+
+
+class MenuItem(Widget):
+
+    def __init__(self, parent: QWidget):
+        QWidget.__init__(self, parent)
