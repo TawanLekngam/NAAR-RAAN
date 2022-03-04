00daee8872f0a0032ca359d8dab07eabd4a46e0f
neneprm <s.prima@hotmail.com>
2022-03-04 21:20:40 +0700

    Create Theme

---
+++ views/themes/theme.py
@@ -0,0 +1,22 @@
+from PySide6 import QFont
+
+class Theme:
+    #Font
+    DONGLE_REGULAR_65 = QFont("Dongle")
+    DONGLE_REGULAR_65.setPixelSize(65)
+
+    DONGLE_BOLD_80 = QFont("Dongle")
+    DONGLE_BOLD_80.setPixelSize(80)
+    DONGLE_BOLD_80.setBold(True)
+
+    DONGLE_BOLD_65 = QFont("Dongle")
+    DONGLE_BOLD_65.setPixelSize(65)
+    DONGLE_BOLD_80.setBold(True)
+
+    #Colors
+    DARK_BROWN = "3B2816"
+    LIGHT_BROWN = "754926"
+    CREAM = "D8B797"
+    EGG_WHITE = "F9F5F0"
+
+
