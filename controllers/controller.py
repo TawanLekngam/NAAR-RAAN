ccaa971d159543c5e3b56b2b441bdc4a156abc0b
Tawan <tawancraft@outlook.com>
2022-04-05 16:46:33 +0700

    Update controller.py
    + connect view and model (Login)

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,22 +5,27 @@ from models import *
 
 
 class Controller:
-    def __init__(self, view: QWidget = None, model: Model= None):
+    def __init__(self, view: QWidget = None, model: Model = None):
         self.view = view
         self.model = model
 
     def show_page(self) -> None:
         self.view.showFullScreen()
 
+
 class LogInController(Controller):
     def __init__(self):
-        Controller.__init__(self, Log_in(), None)
-        self.view.login_button_emit(lambda: print("Clicked"))
+        Controller.__init__(self, LogInPage(), LogInModel())
+        self.view: LogInPage
+        self.model: LogInModel
+        self.view.login_button_emit(self.logIn)
+        self.show_page()
+
+    def logIn(self):
+        if self.model.login_verify(self.view.lineEdit_username.text(),self.view.lineEdit_password.text()):
+            self.view.close()
 
 class MenuOrderController(Controller):
     def __init__(self):
         Controller.__init__(self, MenuOrderPage(), MenuOrderModel())
         self.show_page()
-
-
-
