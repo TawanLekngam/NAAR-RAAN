aa2ef174aa79f2ab00b0cc468d9a9f90a63393eb
Tawan <tawancraft@outlook.com>
2022-04-05 16:28:18 +0700

    update main and test application

--- application.py
+++ application.py
@@ -1,3 +1,7 @@
+"""
+    
+"""
+
 import sys
 from PySide6.QtWidgets import (QApplication)
 from controllers import *
@@ -6,11 +10,7 @@ from views.order_details import Order_details
 
 def main() -> int:
     app = QApplication(sys.argv)
-    # win = LogInController()       # For test LogIn Page
-    # win = MenuOrderController()     # For test Menu Order Page
-
     od = Order_details()
-
     return app.exec()
 
 
--- application_test.py
+++ application_test.py
@@ -1,5 +1,12 @@
-from data import *
+import sys
+from PySide6.QtWidgets import (QApplication)
+
+from controllers import *
 
 if __name__ == "__main__":
-    data_access = AppDAO.get_dao("user")
-    print(type(data_access).__name__)
+    # data_access = AppDAO.get_dao("logentry")
+    # print(type(data_access).__name__)
+
+    app = QApplication(sys.argv)
+    w = MenuOrderController()
+    sys.exit(app.exec_())
