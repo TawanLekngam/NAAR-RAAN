d887717aa9da9b5eefc3c663752b70032d7d160f
Tawan <tawancraft@outlook.com>
2022-04-06 10:47:22 +0700

    Update application.py
    + create Application class

--- application.py
+++ application.py
@@ -4,15 +4,41 @@
 
 import sys
 from PySide6.QtWidgets import (QApplication)
-from controllers import *
-from views.order_details import Order_details
+
+from controllers.controller import (Controller, LogInPage, MenuOrderPage)
+from views.view import (LoginView, MenuOrderView)
+from models.model import (LoginModel, MenuOrderModel)
+from data.data_classes import (User)
 
 
-def main() -> int:
-    app = QApplication(sys.argv)
-    od = Order_details()
-    return app.exec()
+class Application:
+    __current_user: User
+    __current_page: Controller
+
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.__current_user = None
+
+        self.__login_page = LogInPage(
+            LoginView(),
+            LoginModel())
+
+        self.__menuorder_page = MenuOrderPage(
+            MenuOrderView(),
+            MenuOrderModel())
+
+
+        self.__current_page = self.__login_page
+
+    def start(self) -> None:
+        self.__current_page.show_page()
+        sys.exit(self.app.exec_())
+
+    def update(self) -> None:
+        self.__current_page.update()
+
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    app = Application()
+    app.start()
