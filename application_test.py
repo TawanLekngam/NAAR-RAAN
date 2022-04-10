295df53adf0f91589e3d4e2dc81ba922a042a511
Tawan <tawancraft@outlook.com>
2022-04-10 23:05:49 +0700

    Update application_test.py

--- application_test.py
+++ application_test.py
@@ -1,12 +1,47 @@
+"""
+    tester file
+"""
+
 import sys
 from PySide6.QtWidgets import (QApplication)
 
-from controllers import *
+from controllers.controller import *
+from views.view import *
+from models.model import *
+from data.data_classes import (User)
+
+
+class Application:
+    __current_user: User
+    __current_page: Controller
+
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.__current_user = None
+
+        self.__login_page = LogInPage(
+            LoginView(),
+            LoginModel())
+
+        self.__menuorder_page = MenuOrderPage(
+            MenuOrderView(),
+            MenuOrderModel())
+
+        """
+            start page will show 
+        """
+        self.__current_page = self.__login_page
+
+    def start(self) -> None:
+        self.__current_page.show_page()
+        sys.exit(self.app.exec_())
+
+    def update(self) -> None:
+        self.__current_page.update()
+
+
+
 
 if __name__ == "__main__":
-    # data_access = AppDAO.get_dao("logentry")
-    # print(type(data_access).__name__)
-
-    app = QApplication(sys.argv)
-    w = LogInController()
-    sys.exit(app.exec_())
+    app = Application()
+    app.start()
