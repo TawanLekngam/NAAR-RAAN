2a75832d7df649b437720275cf0368cf510080b1
Tawan <tawancraft@outlook.com>
2022-05-26 23:15:10 +0700

    add LoginPage

--- src/controllers.py
+++ src/controllers.py
@@ -1,23 +1,44 @@
+from abc import ABC
+from datetime import date
 from PySide6.QtWidgets import QWidget
 
-from src.model import *
-from src.view import *
+from .application import Application
+from .models import *
+from .views import *
 
 
 class Controller:
-    def __init__(self, v: QWidget = None, m: Model = None):
-        self.__v = v  # view
-        self.__m = m  # model
+    view: QWidget
+    model: Model
 
-    def get_page(self) -> QWidget:
-        return self.v
+    def __init__(self, view: QWidget = None, model: Model = None):
+        self.view = view
+        self.model = model
 
-    def show_page(self) -> None:
-        self.v.show()
+    def open_page(self):
+        if self.view is None:
+            return
+        self.view.show()
 
-    def showf_page(self) -> None:
-        self.v.showFullScreen
 
-    def update_view(self) -> None:
-        self.v.update()
+class LoginPage(Controller):
+    view: LoginView
+    model: LoginModel
 
+    def __init__(self, root: Application, view: QWidget, model: Model):
+        super().__init__(view, model)
+        self.__root: Application = root
+
+    def verify_login(self):
+        username = self.view.get_username()
+        password = self.view.get_password()
+        self.model.get_input(username, password)
+        if self.model.is_valid():
+            self.model.set_current_user(username)
+            self.set_current_user(self.get_current_user())
+
+    def get_current_user(self) -> User:
+        return self.model.get_current_user()
+
+    def set_current_user(self, user: User):
+        self.root.set_current_user(user)
