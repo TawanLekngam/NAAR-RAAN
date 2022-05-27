b2020dca68751665a3c2d617d90672d2fbe8351e
Tawan <tawancraft@outlook.com>
2022-05-27 17:39:08 +0700

    create HomeModel and AccountModel

--- src/models.py
+++ src/models.py
@@ -42,3 +42,51 @@ class LoginModel(Model):
 
     def get_current_user(self) -> User:
         return self.__current_user
+
+
+class HomeModel(Model):
+
+    def __init__(self, user: User):
+        pass
+
+    def logout(self) -> None:
+        pass
+
+
+class AccountModel(Model):
+    LENGHT_LIMIT = 4
+    __current_user: User
+    __user_dao: UserDAO
+    __log_dao: LogDAO
+
+    def __init__(self, user: User = None):
+        self.__current_user = user
+        self.__user_dao = AppDAO.get_dao("user")
+        self.__log_dao = AppDAO.get_dao("log")
+
+    def get_all_account(self) -> list[User]:
+        return self.__user_dao.get_all_users()
+
+    def create_new_account(self, user: User) -> None:
+        self.__user_dao.add_user(user)
+
+        log = Log(
+            f"ADMIN: {self.__current_user.get_username()} created new account for {user.get_username()}.")
+        self.__log_dao.add_log(log)
+
+    def generate_username(self, fname: str, lname: str) -> str:
+        username = str()
+
+        if len(fname) <= AccountModel.LENGHT_LIMIT:
+            username += fname.lower()
+        else:
+            username += fname[0:AccountModel.LENGHT_LIMIT].lower()
+
+        username += "."
+
+        if len(lname) <= AccountModel.LENGHT_LIMIT:
+            username += lname.lower()
+        else:
+            username += lname[0:AccountModel.LENGHT_LIMIT].lower()
+        
+        return username
