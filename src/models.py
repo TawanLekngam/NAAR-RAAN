517481ba6d1be0a7684b9b39a60e1a662545179a
faynch <83182929+faynch@users.noreply.github.com>
2022-05-28 07:37:23 +0700

    sort class
    Co-Authored-By: Tawan Lekngam <70558691+TawanLekngam@users.noreply.github.com>

--- src/models.py
+++ src/models.py
@@ -10,67 +10,6 @@ class Model(ABC):
     pass
 
 
-class LoginModel(Model):
-
-    def __init__(self):
-        self.user_dao: UserDAO = AppDAO.get_dao("user")
-        self.__current_user: User = None
-        self.__current_username = str()
-        self.__current_password = str()
-        self.valid_login = False
-
-    def get_input(self, username: str, password: str) -> None:
-        self.__current_username = username
-        self.__current_password = password
-
-    def retrive_user(self, username: str) -> None:
-        self.__current_user = self.user_dao.get_user_by_username(username)
-
-    def verify_login(self) -> None:
-        if self.__current_user is None:
-            self.valid_login = False
-            return
-
-        self.valid_login = (self.__current_username == self.__current_user.get_username()
-                            and self.__current_password == self.__current_user.get_password())
-
-    def is_valid(self) -> bool:
-        return self.valid_login
-
-    def set_current_user(self, username: str) -> None:
-        self.__current_user = self.user_dao.get_user_by_username(username)
-
-    def get_current_user(self) -> User:
-        return self.__current_user
-
-
-class HomeModel(Model):
-
-    def __init__(self):
-        pass
-
-    def logout(self) -> None:
-        pass
-
-
-class OrderModel(Model):
-    __drink_dao: DrinkDAO
-    __bakery_dao: BakeryDAO
-    __receipt_dao: ReceiptDAO
-
-    def __init__(self):
-        self.__drink_dao = AppDAO.get_dao("drink")
-        self.__bakery_dao = AppDAO.get_dao("bakery")
-        self.__receipt_dao = AppDAO.get_dao("receipt")
-
-    def get_all_products(self) -> list:
-        products = list()
-        products.extend(self.__drink_dao.get_all_drinks())
-        products.extend(self.__bakery_dao.get_all_bakeries())
-        products.sort(key=lambda x: x.name)
-        return products
-
-
 class AccountModel(Model):
     LENGHT_LIMIT = 4
     __current_user: User
@@ -110,6 +49,49 @@ class AccountModel(Model):
         return username
 
 
+class HomeModel(Model):
+
+    def __init__(self):
+        pass
+
+    def logout(self) -> None:
+        pass
+
+
+class LoginModel(Model):
+
+    def __init__(self):
+        self.user_dao: UserDAO = AppDAO.get_dao("user")
+        self.__current_user: User = None
+        self.__current_username = str()
+        self.__current_password = str()
+        self.valid_login = False
+
+    def get_input(self, username: str, password: str) -> None:
+        self.__current_username = username
+        self.__current_password = password
+
+    def retrive_user(self, username: str) -> None:
+        self.__current_user = self.user_dao.get_user_by_username(username)
+
+    def verify_login(self) -> None:
+        if self.__current_user is None:
+            self.valid_login = False
+            return
+
+        self.valid_login = (self.__current_username == self.__current_user.get_username()
+                            and self.__current_password == self.__current_user.get_password())
+
+    def is_valid(self) -> bool:
+        return self.valid_login
+
+    def set_current_user(self, username: str) -> None:
+        self.__current_user = self.user_dao.get_user_by_username(username)
+
+    def get_current_user(self) -> User:
+        return self.__current_user
+
+
 class LogModel(Model):
     __log_dao: LogDAO
 
@@ -120,6 +102,31 @@ class LogModel(Model):
         return self.__log_dao.get_all_logs()
 
 
-if __name__ == "__main__":
-    x = OrderModel()
-    x.get_all_products()
+class MenuModel(Model):
+
+    def __init__(self):
+        pass
+
+
+class OrderModel(Model):
+    __drink_dao: DrinkDAO
+    __bakery_dao: BakeryDAO
+    __receipt_dao: ReceiptDAO
+
+    def __init__(self):
+        self.__drink_dao = AppDAO.get_dao("drink")
+        self.__bakery_dao = AppDAO.get_dao("bakery")
+        self.__receipt_dao = AppDAO.get_dao("receipt")
+
+    def get_all_products(self) -> list:
+        products = list()
+        products.extend(self.__drink_dao.get_all_drinks())
+        products.extend(self.__bakery_dao.get_all_bakeries())
+        products.sort(key=lambda x: x.name)
+        return products
+
+
+class ReceiptModel(Model):
+
+    def __init__(self):
+        pass
